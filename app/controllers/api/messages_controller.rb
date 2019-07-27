768f0bfd1f4d77438135582b8c4d59eaefa64808
yasuharus <harukuroa@icloud.com>
2019-07-27 18:44:05 +0900

    Automatic update implementation

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,10 +1,28 @@
+# class Api::MessagesController < ApplicationController
+#   def index
+#     @group = Group.find(params[:group_id])
+
+#     respond_to do |format|
+#       format.html
+#       format.json{ @messages = Message.where("id > ? and group_id = ? ", params[:id], @group.id) }
+#       binding.pry
+#     end
+
+#   end
+# end
+
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @group = Group.find(params[:group_id])
+   
+       @messages = Message.where('id > ? and group_id = ? ', params[:id], @group.id) 
 
-    respond_to do |format|
-      format.html
-      format.json{ @new_message = Message.where("id > ?", params[:message][:id]) }
-    end
+    # @messages = Message.includes(:user).where('id > ?', params[:id])
+    # binding.pry
   end
+
+  # def index
+  #   @messages = Message.includes(:user).where('id > ?', params[:id])
+  # end
+
 end
